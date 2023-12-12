9dad252eaa53b49c610014f6b1d8094efeadc280
vadim <vadim@XCSENON>
2023-12-12 15:55:36 +0200

    Добавьте файлы проекта.

---
+++ odn/odn.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+class Singleton {
+public:
+    static Singleton& getInstance() {
+        static Singleton instance;
+        return instance;
+    }
+
+    void showMessage() {
+        std::cout << "Hello from Singleton!" << std::endl;
+    }
+
+private:
+    Singleton() {}
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+};
+
+int main() {
+    Singleton& singleton = Singleton::getInstance();
+    singleton.showMessage();
+
+    return 0;
+}
